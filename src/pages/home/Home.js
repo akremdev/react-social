e33f19067fc7b4314b03910063b47e5b97f2743f
akremdev <akremdev@gmail.com>
2021-04-29 14:19:26 +0100

    topbar commit

---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Topbar from '../../components/topbar/Topbar'
+import './home.css'
+
+function Home  () {
+    return (
+        <div>
+            <Topbar />
+        </div>
+    )
+}
+
+
+export default Home;
